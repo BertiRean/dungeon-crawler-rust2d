8c3e818e9afa1e6b4b7a379304ad73852038c498
BertiRean <tplaza15@gmail.com>
2023-07-03 12:15:07 -0300

    Core: Changed movement updates to Command buffer way

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,10 +1,11 @@
-use crate::{prelude::*, turn_state};
+use crate::{prelude::*};
 
 mod player_input;
 mod map_render;
 mod entity_render;
 mod collisions;
 mod random_move;
+mod movement;
 mod end_turn;
 
 pub fn build_input_scheduler() -> Schedule{
@@ -19,6 +20,8 @@ pub fn build_input_scheduler() -> Schedule{
 pub fn build_player_scheduler() -> Schedule
 {
   Schedule::builder()
+  .add_system(movement::movement_system())
+  .flush()
   .add_system(collisions::collisions_system())
   .flush()
   .add_system(map_render::map_render_system())
@@ -32,21 +35,12 @@ pub fn build_monster_scheduler() -> Schedule
   Schedule::builder()
   .add_system(random_move::random_move_system())
   .flush()
+  .add_system(movement::movement_system())
+  .flush()
   .add_system(collisions::collisions_system())
   .flush()
   .add_system(map_render::map_render_system())
   .add_system(entity_render::entity_render_system())
   .add_system(end_turn::end_turn_system())
-  .build()
-}
-
-pub fn build_scheduler() -> Schedule {
-  Schedule::builder()
-  .add_system(player_input::player_input_system())
-  .add_system(collisions::collisions_system())
-  .flush()
-  .add_system(map_render::map_render_system())
-  .add_system(entity_render::entity_render_system())
-  .add_system(random_move::random_move_system())
   .build()
 }
